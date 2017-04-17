7c6c49d96485b40251a0cbb10b80c2a77aeebb46
yzchair <26429336@qq.com>
2017-04-17 21:41:53 +0800

    问问
    改改看

--- src/main/java/org/b3log/solo/api/symphony/ArticleReceiver.java
+++ src/main/java/org/b3log/solo/api/symphony/ArticleReceiver.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * Copyright (c) 2010-2017, b3log.org & hacpai.com
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,6 +12,7 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+reehehh34h3
  */
 package org.b3log.solo.api.symphony;
 
